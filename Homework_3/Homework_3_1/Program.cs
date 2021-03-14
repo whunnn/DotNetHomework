c52b732c64c4f94f3d58979ae4884b408a24a40a
whunnn <1416862880@qq.com>
2021-03-14 18:24:15 +0800

    HomeWork_3

---
+++ Homework_3/Homework_3_1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3_1
+{
+    public abstract class Shape
+    {
+        public abstract void Show();
+        public abstract double CalculateArea();
+        public abstract bool IsLegal();
+    }
+    public class Rectangle : Shape
+    {
+        protected double length;
+        protected double width;
+        public Rectangle(double length,double width)
+        {
+            this.length = length;
+            this.width = width;
+            
+        }
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+        public double Width
+        {
+            get => width;
+            set => width = value;
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"矩形：长为{length} 宽为{width}");
+        }
+        public override double CalculateArea()
+        {
+            return width * length;
+        }
+        public override bool IsLegal()
+        {
+            return length > 0 && width > 0;
+
+        }
+    }
+    public class Square : Rectangle
+    {
+        public Square(double length) : base(length, length)
+        {
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"正方形：边长为{length}");
+        }
+    }
+    public class Triangle : Shape
+    {    
+        //三角形的三条边
+        private double a;
+        private double b;
+        private double c;
+        public Triangle(double a,double b,double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"三角形：三条边分别为{a} {b} {c}"); 
+        }
+        public override double CalculateArea()
+        {
+            //海伦公式计算三角形面积
+            return ( Math.Sqrt((a + b + c)*(a + b - c)*(a + c - b)*(b + c - a))/4);
+        }
+        public override bool IsLegal()
+        {
+            return (a + b > c && a + c > b && b + c > a);
+        }
+    }
+    class Test
+    {   //测试图形函数
+        public static void TestShape(Shape shape)
+        {
+            if (shape.IsLegal())
+                Console.WriteLine($"该图形合法，且其面积为{shape.CalculateArea()}");
+            else Console.WriteLine("此图形不合法");         
+        }
+        static void Main(string[] args)
+        {   //矩形
+            Console.WriteLine("实例化一个矩形，请输入其长和宽（中间用空格隔开）：");
+            string s = Console.ReadLine();
+            string[] data = s.Split(' ');
+            double length = double.Parse(data[0]);
+            double width = double.Parse(data[1]);
+            Rectangle rect = new Rectangle(length, width);
+            TestShape(rect);
+            //正方形
+            Console.WriteLine("实例化一个正方形，请输入其边长：");
+            length = double.Parse(Console.ReadLine());
+            Square square = new Square(length);
+            TestShape(square);
+            //三角形
+            Console.WriteLine("实例化一个三角形，请输入其三边长（中间用空格隔开）：");
+            s = Console.ReadLine();
+            data = s.Split(' ');
+            double a= double.Parse(data[0]),b=double.Parse(data[1]),c= double.Parse(data[2]);
+            Triangle triangle = new Triangle(a, b, c);
+            TestShape(triangle);
+
+        }
+    }
+}
---
+++ Homework_3/Homework_3_2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework_3_1;
+
+namespace Homework_3_2
+{
+    public class ShapeFactory
+    {
+        /*
+         *由于程序进程过快生成的随机数有很多重复的
+         *查找资料后使用加密服务提供程序 (CSP) 提供的实现来实现加密随机数生成器（RNG）
+         */
+        static int GetRandomSeed()
+        {
+
+            byte[] bytes = new byte[4];
+
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider(); rng.GetBytes(bytes);
+
+            return BitConverter.ToInt32(bytes, 0);
+
+        }
+        public static Shape CreatShape(string shape)
+        {
+            Random rd = new Random(GetRandomSeed());
+            Shape mshape = null;
+            switch (shape)
+            {
+                case "rectangle":
+                    do
+                    {
+                        mshape = new Rectangle(rd.Next(0,20), rd.Next(0, 20));
+                    } while (!mshape.IsLegal());
+                    return mshape;
+                case "square":
+                    do
+                    {
+                        mshape = new Square(rd.Next(0, 20));
+                    } while (!mshape.IsLegal());
+                    return mshape;
+                case "triangle":
+                    do
+                    {
+                        mshape = new Triangle(rd.Next(0, 20), rd.Next(0, 20), rd.Next(0, 20));
+                    } while (!mshape.IsLegal());
+                    return mshape;
+                default:
+                    return null;
+            }
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                switch (random.Next() % 3)
+                {
+                    case 0:
+                        shapes[i]=ShapeFactory.CreatShape("rectangle");
+                        break;
+                    case 1:
+                        shapes[i] = ShapeFactory.CreatShape("square");
+                        break;
+                    case 2:
+                        shapes[i] = ShapeFactory.CreatShape("triangle");
+                        break;
+                }
+            }
+            double sumArea = 0;
+            for(int i=0;i<shapes.Length;i++){
+                shapes[i].Show();
+                sumArea += shapes[i].CalculateArea();
+            }
+            Console.WriteLine($"这些图形的总面积为：{sumArea}");
+        }
+    }
+}
