3bbd209e36413aad7c3ab5b3a37c7d562e7595c5
whunnn <1416862880@qq.com>
2021-03-26 00:03:11 +0800

    Homework_4

---
+++ Homework_4/Homework_4_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_1
+{
+    class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head
+            {
+                get => head;
+            }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public static void ForEach(GenericList<T> genericlist, Action<T> f)
+            {
+                for (Node<T> node = genericlist.Head; node != null; node = node.Next)
+                {
+                    f(node.Data);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            GenericList<int> List = new GenericList<int>();
+            Random rd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                List.Add(rd.Next(1, 50));
+            }
+            int sum = 0, max = 0, min = int.MaxValue;
+            Console.WriteLine("随机产生的十个链表元素为：");
+            GenericList<int>.ForEach(List, m => Console.Write($"{m} "));
+            Console.WriteLine("");
+            GenericList<int>.ForEach(List, m => sum += m);
+            GenericList<int>.ForEach(List, m => max = m < max ? max : m);
+            GenericList<int>.ForEach(List, m => min = m > min ? min : m);
+            Console.WriteLine($"最大值为{max} 最小值为{min} 总和为{sum} 平均值为{sum / 10.0}");
+        }
+    }
+}
---
+++ Homework_4/Homework_4_2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Homework_4_2
+{
+    public class Clock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+        private int h;
+        private int m;
+        private int s;
+        public Clock(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public Clock()
+        {
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
+        public event Action Tick;
+        public event Action Alarm;
+        public void Tick1()
+        {
+            second = second + 1;
+            if (second >= 60)
+            {
+                minute += 1;
+                second = 0;
+            }
+            if (minute >= 60)
+            {
+                hour += 1;
+                minute = 0;
+            }
+            if (hour >= 24)
+                hour = 0;
+        }
+        public void ShowTime()
+        {
+            Console.WriteLine($"{hour}:{minute}:{second}");
+        }
+
+        public void SetAlarm()
+        {
+            Console.WriteLine("请设置闹铃的时间(分别输入时钟分钟秒钟)：");
+            h = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            s = int.Parse(Console.ReadLine());
+            Console.WriteLine($"闹铃的时间为：{h}:{m}:{s}");
+        }
+
+        public void Alarm1()
+        {
+            if (h == hour && m == minute && s == second)
+                Console.WriteLine("Clock Alarming!");
+        }
+
+        public void StartClock()
+        {
+            while (true)
+            {
+                Tick();
+                Alarm();
+                Thread.Sleep(1000);
+            }
+        }
+        class Form
+        {
+            public Clock clock = new Clock(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            public Form()
+            {
+                clock.Tick += clock.ShowTime;
+                clock.Tick += clock.Tick1;
+                clock.Alarm += clock.Alarm1;
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Form form = new Form();
+                Console.Write("当前时间为:");
+                form.clock.ShowTime();
+                form.clock.SetAlarm();
+                form.clock.StartClock();
+            }
+        }
+    }
+}
